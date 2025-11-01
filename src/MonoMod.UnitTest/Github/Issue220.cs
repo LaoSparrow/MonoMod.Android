d6b1d6471af6c767a67ab4c636c75e226bbfce36
wuke32767 <2446214230@qq.com>
2025-11-01 22:56:36 +0800

    update test case to actually reproduce the issue

--- src/MonoMod.UnitTest/Github/Issue220.cs
+++ src/MonoMod.UnitTest/Github/Issue220.cs
@@ -2,9 +2,11 @@ extern alias New;
 using New::MonoMod.RuntimeDetour;
 using System;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Runtime.CompilerServices;
 using Xunit;
 using Xunit.Abstractions;
+using MonoMod.Cil;
 
 namespace MonoMod.UnitTest.Github
 {
@@ -17,7 +19,7 @@ namespace MonoMod.UnitTest.Github
         private class DrawTarget
         {
             public bool IsDrawn { get; private set; }
-            
+
             [MethodImpl(MethodImplOptions.NoInlining)]
             public void DrawMenu(GameTime gameTime)
             {
@@ -30,31 +32,49 @@ namespace MonoMod.UnitTest.Github
             // Minimal mock to match the scenario
         }
 
+        class GCCallback(Action callback)
+        {
+            ~GCCallback()
+            {
+                callback();
+            }
+
+            public static void Register(Action callback)
+            {
+                _ = new GCCallback(callback);
+            }
+        }
+
+
         [Fact]
         public void ConcurrentILHooksDoNotCauseRaceCondition()
         {
-            for (var i = 0; i < 10; i++)
-            {
-                var target = new DrawTarget();
-                
-                // Simulate multiple hooks being added concurrently
-                Parallel.For(0, 10, i =>
-                {
-                    using (var hook = new Hook(typeof(DrawTarget).GetMethod(nameof(DrawTarget.DrawMenu)),
-                        (Action<Action<DrawTarget, GameTime>, DrawTarget, GameTime>)((orig, self, gameTime) =>
-                        {
-                            // Original method call
-                            orig(self, gameTime);
-                        })))
-                    {
-                        // Call the hooked method
-                        target.DrawMenu(new GameTime());
-                    }
-                });
+            var method = typeof(DrawTarget).GetMethod(nameof(DrawTarget.DrawMenu));
+            var target = new DrawTarget();
 
-                // Verify that the method was drawn
-                Assert.True(target.IsDrawn, "Method should have been drawn without raising an exception");
-            }
+            using ILHook hook = new(method, i =>
+            {
+                ILCursor ic = new(i);
+                ic.EmitDelegate(() => { });
+            });
+            using ILHook whereRaceConditionHappens = new(method, _ =>
+            {
+                var done = false;
+                GCCallback.Register(() => done = true);
+
+                var retryCount = 0;
+                while (!done)
+                {
+                    GC.Collect(2, GCCollectionMode.Forced);
+                    Thread.Sleep(1);
+                    retryCount++;
+                    if (retryCount > 100)
+                    {
+                        throw new InvalidOperationException("if GCCallback is not triggered, this test case can't work.");
+                    }
+                }
+                Task.Run(() => target.DrawMenu(new GameTime())).Wait();
+            });
         }
     }
 }
